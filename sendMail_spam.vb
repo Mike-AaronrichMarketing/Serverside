8e1fb3e7f0b1431df3f561ed20cad505f5a09b2a
Mike-AaronrichMarketing <109161216+Mike-AaronrichMarketing@users.noreply.github.com>
2022-12-28 14:14:31 -0600

    Create sendMail_spam.vb

---
+++ sendMail_spam.vb
@@ -0,0 +1,31 @@
+' This will allow you to block email addresses from the email value of the text field.
+'apply If ElseIf statement with the blacklist emails.
+
+
+
+If email.value.Contains("ericjonesmyemail@gmail.com") Then
+			Response.Redirect("error")
+		ElseIf email.value.Contains("eric.jones.z.mail@gmail.com") Then
+			Response.Redirect("error")
+
+		Else
+
+
+			Dim smtp As New SmtpClient()
+
+
+			Try
+				smtp.Credentials = New _
+Net.NetworkCredential("PM-T-surfacespcb-com-VHgtIrAYueMckoV", "Uac3vubodHqySMf0izn3KGYcIEvn7EMkt2ch")
+				smtp.Port = 587
+				smtp.Host = "smtp.postmarkapp.com"
+				smtp.Send(mail)
+				Response.Redirect("thanks")
+			Catch ehttp As System.Web.HttpException
+				Response.Redirect("error?ret=1")
+			End Try
+
+
+		End If
+		rAlert.Attributes.Add("style", "display: block;color:red;")
+	End If
